f52d9daeb38203ee4abd68328b9cc5a47c7e40e8
mskmani09 <mkulk1@unh.newhaven.edu>
2020-10-30 18:25:00 -0400

    Product Listing Controller - Navigation from Buyer Home page to product listing page and filter data with category that is being clicked.

--- app/src/main/java/edu/newhaven/virtualfarmersmarket/ProductListingBuyerViewHolder.kt
+++ app/src/main/java/edu/newhaven/virtualfarmersmarket/ProductListingBuyerViewHolder.kt
@@ -6,4 +6,8 @@ import androidx.recyclerview.widget.RecyclerView
 
 class ProductListingBuyerViewHolder(view: View): RecyclerView.ViewHolder(view) {
     var productName: TextView = view.findViewById(R.id.tv_productListingVhProductName)
+    var productDescription: TextView = view.findViewById(R.id.tv_productListingVhDescription)
+    var productPrice: TextView = view.findViewById(R.id.tv__productListingVhPrice)
+    var availableQuantity: TextView = view.findViewById(R.id.tv_productListingVhQuantity)
+    var categoryName: TextView = view.findViewById(R.id.tv_productListingVhCategoryName)
 }
--- app/src/main/java/edu/newhaven/virtualfarmersmarket/ProductListingForBuyer.kt
+++ app/src/main/java/edu/newhaven/virtualfarmersmarket/ProductListingForBuyer.kt
@@ -3,6 +3,7 @@ package edu.newhaven.virtualfarmersmarket
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.firebase.ui.firestore.FirestoreRecyclerAdapter
@@ -18,18 +19,27 @@ class ProductListingForBuyer : AppCompatActivity() {
     private val db_productListingBuyer = FirebaseFirestore.getInstance()
     private var adapter : FirestoreRecyclerAdapter<Product, ProductListingBuyerViewHolder>? = null
 
+    private lateinit var categoryFilterView: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_product_listing_for_buyer)
 
+        val intent = intent
+        val categoryFilter = intent.getStringExtra("CategoryClicked")
+
+        categoryFilterView = findViewById(R.id.tv_categoryNamePL)
+
         val query: Query = db_productListingBuyer
             .collection("products")
-            //.whereEqualTo("category","Fruits")
-            .orderBy ("product")
+            .whereEqualTo("category", categoryFilter)
+            .orderBy ("price")
 
         val options: FirestoreRecyclerOptions<Product> = FirestoreRecyclerOptions.Builder<Product>()
             .setQuery(query, Product::class.java)
             .build()
+
+        categoryFilterView.setText(categoryFilter)
 
         adapter = object : FirestoreRecyclerAdapter<Product, ProductListingBuyerViewHolder>(options){
             override fun onBindViewHolder(
@@ -38,6 +48,10 @@ class ProductListingForBuyer : AppCompatActivity() {
                 model: Product
             ) {
                 holder.productName.text = model.product
+                holder.productDescription.text = model.description
+                holder.productPrice.text = model.price.replace("$","")
+                holder.availableQuantity.text = model.quantity
+                holder.categoryName.text = model.category
             }
 
             override fun onCreateViewHolder(
