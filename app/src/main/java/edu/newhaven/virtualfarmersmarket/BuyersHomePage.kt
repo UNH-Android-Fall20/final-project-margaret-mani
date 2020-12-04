5d1cceea07d6e5574f5de71f51c07579d62cf8aa
mskmani09 <mkulk1@unh.newhaven.edu>
2020-12-04 04:03:56 -0500

    Moved location code to registration page so that location is updated only once.
    Also fixed layout issue for registration page.
    Also sellers location is used during add new product.

--- app/src/main/java/edu/newhaven/virtualfarmersmarket/BuyersHomePage.kt
+++ app/src/main/java/edu/newhaven/virtualfarmersmarket/BuyersHomePage.kt
@@ -15,102 +15,102 @@ import de.hdodenhof.circleimageview.CircleImageView
 
 class BuyersHomePage : AppCompatActivity() {
 
-  private val TAG = javaClass.name
+    private val TAG = javaClass.name
 
-  private val db = FirebaseFirestore.getInstance()
-  private var auth = FirebaseAuth.getInstance()  //Needed to check login
+    private val db = FirebaseFirestore.getInstance()
+    private var auth = FirebaseAuth.getInstance()  //Needed to check login
 
-  private lateinit var categoryOne: TextView
-  private lateinit var categoryOneImage: CircleImageView
-  private lateinit var categoryTwo: TextView
-  private lateinit var categoryTwoImage: CircleImageView
-  private lateinit var categoryThree: TextView
-  private lateinit var categoryThreeImage: CircleImageView
-  private lateinit var categoryFour: TextView
-  private lateinit var categoryFourImage: CircleImageView
-  private lateinit var categoryFive: TextView
-  private lateinit var categoryFiveImage: CircleImageView
+    private lateinit var categoryOne: TextView
+    private lateinit var categoryOneImage: CircleImageView
+    private lateinit var categoryTwo: TextView
+    private lateinit var categoryTwoImage: CircleImageView
+    private lateinit var categoryThree: TextView
+    private lateinit var categoryThreeImage: CircleImageView
+    private lateinit var categoryFour: TextView
+    private lateinit var categoryFourImage: CircleImageView
+    private lateinit var categoryFive: TextView
+    private lateinit var categoryFiveImage: CircleImageView
 
-  private lateinit var categoryList: MutableList<String>
+    private lateinit var categoryList: MutableList<String>
 
-  private lateinit var bottom_navigation_menu: BottomNavigationView
+    private lateinit var bottom_navigation_menu: BottomNavigationView
 
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_buyers_home_page)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_buyers_home_page)
 
-    categoryOne = findViewById(R.id.tv_categoryOne)
-    categoryOneImage = findViewById(R.id.iv_categoryOneImage)
-    categoryTwo = findViewById(R.id.tv_categoryTwo)
-    categoryTwoImage = findViewById(R.id.iv_categoryTwoImage)
-    categoryThree = findViewById(R.id.tv_categoryThree)
-    categoryThreeImage = findViewById(R.id.iv_categoryThreeImage)
-    categoryFour = findViewById(R.id.tv_categoryFour)
-    categoryFourImage = findViewById(R.id.iv_categoryFourImage)
-    categoryFive = findViewById(R.id.tv_categoryFive)
-    categoryFiveImage = findViewById(R.id.iv_categoryFiveImage)
-    bottom_navigation_menu = findViewById(R.id.bottom_navigation_view)
+        categoryOne = findViewById(R.id.tv_categoryOne)
+        categoryOneImage = findViewById(R.id.iv_categoryOneImage)
+        categoryTwo = findViewById(R.id.tv_categoryTwo)
+        categoryTwoImage = findViewById(R.id.iv_categoryTwoImage)
+        categoryThree = findViewById(R.id.tv_categoryThree)
+        categoryThreeImage = findViewById(R.id.iv_categoryThreeImage)
+        categoryFour = findViewById(R.id.tv_categoryFour)
+        categoryFourImage = findViewById(R.id.iv_categoryFourImage)
+        categoryFive = findViewById(R.id.tv_categoryFive)
+        categoryFiveImage = findViewById(R.id.iv_categoryFiveImage)
+        bottom_navigation_menu = findViewById(R.id.bottom_navigation_view)
 
-    db.collection("categories").get()
-      .addOnSuccessListener { documents ->
-        this.categoryList = mutableListOf()
-        for (document in documents) {
-          document.getString("category")?.let { categoryList.add(it) }
-          Log.d(TAG, "${document.id} => ${document.getString("category")}")
+        db.collection("categories").get()
+            .addOnSuccessListener { documents ->
+                this.categoryList = mutableListOf()
+                for (document in documents) {
+                    document.getString("category")?.let { categoryList.add(it) }
+                    Log.d(TAG, "${document.id} => ${document.getString("category")}")
+                }
+                categoryOne.setText(categoryList[0])
+                categoryTwo.setText(categoryList[1])
+                categoryThree.setText(categoryList[2])
+                categoryFour.setText(categoryList[3])
+                categoryFive.setText(categoryList[4])
+            }
+
+        categoryOne.setOnClickListener{view: View -> productListing(categoryOne.text)}
+        categoryOneImage.setOnClickListener {view: View -> productListing(categoryOne.text)}
+        categoryTwo.setOnClickListener{view: View -> productListing(categoryTwo.text)}
+        categoryTwoImage.setOnClickListener {view: View -> productListing(categoryTwo.text)}
+        categoryThree.setOnClickListener{view: View -> productListing(categoryThree.text)}
+        categoryThreeImage.setOnClickListener {view: View -> productListing(categoryThree.text)}
+        categoryFour.setOnClickListener{view: View -> productListing(categoryFour.text)}
+        categoryFourImage.setOnClickListener {view: View -> productListing(categoryFour.text)}
+        categoryFive.setOnClickListener{view: View -> productListing(categoryFive.text)}
+        categoryFiveImage.setOnClickListener {view: View -> productListing(categoryFive.text)}
+
+
+        bottom_navigation_menu.selectedItemId = R.id.nav_home
+        bottom_navigation_menu.setOnNavigationItemSelectedListener { item ->
+            var message = ""
+            when(item.itemId) {
+                R.id.nav_sell_home -> {  //also add this above onCreate: private var auth = FirebaseAuth.getInstance()
+                    if (auth.currentUser == null){
+                        val intent = Intent(this, Login::class.java)
+                        startActivity(intent)
+                        Log.d(TAG, "Not logged in")
+                    } else {
+                        val intent = Intent(this, SellersHomePage::class.java)
+                        startActivity(intent)
+                        Log.d(TAG, "User is in")
+                    }
+                }
+                R.id.nav_home -> message = "Home"
+                R.id.nav_settings ->  {
+                    val intent = Intent(this, UserSettings::class.java)
+                    startActivity(intent)
+                }
+                R.id.nav_logout -> {
+                    FirebaseAuth.getInstance().signOut()
+                    firebaseUserID = ""
+                }
+            }
+            Toast.makeText(this, "$message clicked!!", Toast.LENGTH_SHORT).show()
+            return@setOnNavigationItemSelectedListener true
         }
-        categoryOne.setText(categoryList[0])
-        categoryTwo.setText(categoryList[1])
-        categoryThree.setText(categoryList[2])
-        categoryFour.setText(categoryList[3])
-        categoryFive.setText(categoryList[4])
-      }
+    }
 
-    categoryOne.setOnClickListener{view: View -> productListing(categoryOne.text)}
-    categoryOneImage.setOnClickListener {view: View -> productListing(categoryOne.text)}
-    categoryTwo.setOnClickListener{view: View -> productListing(categoryTwo.text)}
-    categoryTwoImage.setOnClickListener {view: View -> productListing(categoryTwo.text)}
-    categoryThree.setOnClickListener{view: View -> productListing(categoryThree.text)}
-    categoryThreeImage.setOnClickListener {view: View -> productListing(categoryThree.text)}
-    categoryFour.setOnClickListener{view: View -> productListing(categoryFour.text)}
-    categoryFourImage.setOnClickListener {view: View -> productListing(categoryFour.text)}
-    categoryFive.setOnClickListener{view: View -> productListing(categoryFive.text)}
-    categoryFiveImage.setOnClickListener {view: View -> productListing(categoryFive.text)}
-
-
-    bottom_navigation_menu.selectedItemId = R.id.nav_home
-    bottom_navigation_menu.setOnNavigationItemSelectedListener { item ->
-    var message = ""
-    when(item.itemId) {
-      R.id.nav_sell_home -> {  //also add this above onCreate: private var auth = FirebaseAuth.getInstance()
-        if (auth.currentUser == null){
-          val intent = Intent(this, Login::class.java)
-          startActivity(intent)
-          Log.d(TAG, "Not logged in")
-        } else {
-            val intent = Intent(this, SellersHomePage::class.java)
-            startActivity(intent)
-            Log.d(TAG, "User is in")
-          }
-        }
-      R.id.nav_home -> message = "Home"
-      R.id.nav_settings ->  {
-        val intent = Intent(this, UserSettings::class.java)
+    private fun productListing(categoryClicked: CharSequence){
+        val intent = Intent(this, ProductListingForBuyer::class.java)
+        intent.putExtra("CategoryClicked", categoryClicked)
         startActivity(intent)
-      }
-      R.id.nav_logout -> {
-        FirebaseAuth.getInstance().signOut()
-        firebaseUserID = ""
-      }
     }
-      Toast.makeText(this, "$message clicked!!", Toast.LENGTH_SHORT).show()
-      return@setOnNavigationItemSelectedListener true
-    }
-  }
-
-  private fun productListing(categoryClicked: CharSequence){
-    val intent = Intent(this, ProductListingForBuyer::class.java)
-    intent.putExtra("CategoryClicked", categoryClicked)
-    startActivity(intent)
-  }
 }
 
