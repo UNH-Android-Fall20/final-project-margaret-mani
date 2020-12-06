f8e6fb9def6ce2845eac0217ea4cea6efd2d3aca
mskmani09 <mkulk1@unh.newhaven.edu>
2020-12-06 00:53:15 -0500

    Added Spinny animation for images and written a listener for BUY button on recycler view  on buyer side.

--- app/src/main/java/edu/newhaven/virtualfarmersmarket/ProductDetailsBuyer.kt
+++ app/src/main/java/edu/newhaven/virtualfarmersmarket/ProductDetailsBuyer.kt
@@ -2,6 +2,8 @@ package edu.newhaven.virtualfarmersmarket
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.swiperefreshlayout.widget.CircularProgressDrawable
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
 import com.google.firebase.ktx.Firebase
 import com.google.firebase.storage.ktx.storage
 import kotlinx.android.synthetic.main.activity_product_details_buyer.*
@@ -13,8 +15,18 @@ class ProductDetailsBuyer : AppCompatActivity() {
 
         val product = intent.getSerializableExtra("SelectedProduct") as? Product ?: return
 
+        val circularProgressDrawable = CircularProgressDrawable(this)
+        circularProgressDrawable.strokeWidth = 5f
+        circularProgressDrawable.centerRadius = 30f
+        circularProgressDrawable.start()
+
         val storageReference = Firebase.storage.getReferenceFromUrl(product.imageLoc)
-        GlideApp.with(this).load(storageReference).into(iv_product_image_PDB)
+        GlideApp
+            .with(this)
+            .load(storageReference)
+            .placeholder(circularProgressDrawable)
+            .transition(DrawableTransitionOptions.withCrossFade())
+            .into(iv_product_image_PDB)
 
         tv_product_name_PDB.text = product.product
         tv_category_PDB.text = product.category
