b84324028cd4d7d0f9cf8787bea063fcacb35e02
mskmani09 <mkulk1@unh.newhaven.edu>
2020-12-07 01:05:54 -0500

    Minor changes.

--- app/src/main/java/edu/newhaven/virtualfarmersmarket/ProductViewHolder.kt
+++ app/src/main/java/edu/newhaven/virtualfarmersmarket/ProductViewHolder.kt
@@ -6,10 +6,10 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
 class ProductViewHolder (view: View) : RecyclerView.ViewHolder(view) {
-  //var productImagePL: ImageView = view.findViewById(R.id.iv_productListingVhPhoto)
-  var name: TextView = view.findViewById(R.id.tv_prodName)
-  var price: TextView = view.findViewById(R.id.tv_prodPrice)
-  var quantity: TextView = view.findViewById((R.id.tv_prodQuantity))
-  var productPicture : ImageView = view.findViewById((R.id.iv_prodPicture))
+    //var productImagePL: ImageView = view.findViewById(R.id.iv_productListingVhPhoto)
+    var name: TextView = view.findViewById(R.id.tv_prodName)
+    var price: TextView = view.findViewById(R.id.tv_prodPrice)
+    var quantity: TextView = view.findViewById((R.id.tv_prodQuantity))
+    var productPicture : ImageView = view.findViewById((R.id.iv_prodPicture))
 
 }
